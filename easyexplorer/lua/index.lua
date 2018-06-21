18f5e6c4c56d7b7b2af013ed774418731eb0eccc
janson <gandancing@gmail.com>
2018-06-21 19:04:28 +0800

    todo for ee

--- easyexplorer/lua/index.lua
+++ easyexplorer/lua/index.lua
@@ -1,20 +1,12 @@
 local nixio = require "nixio"
-local http = require "luci.http"
 local ksutil = require "luci.ksutil"
 
 module("luci.controller.apps.easyexplorer.index", package.seeall)
 
 function index()
 	entry({"apps", "easyexplorer"}, call("action_index"))
-	entry({"apps", "easyexplorer", "status"}, call("action_status"))
 end
 
 function action_index()
     ksutil.shell_action("easyexplorer")
-end
-
-function action_easyexplorer_status()
-    http.prepare_content("text/plain; charset=utf-8")
-    reader = ksutil.popen("/koolshare/scripts/easyexplorer-config.sh status", nil)
-    luci.ltn12.pump.all(reader, luci.http.write)
 end
--- easyexplorer/scripts/config.sh
+++ easyexplorer/scripts/config.sh
@@ -5,50 +5,46 @@
 . /koolshare/scripts/uci.sh
 
 on_get() {
-    easyexplorer_status=`pidof easyexplorer|wc -w`
+    local easyexplorer_enabled
+    local easyexplorer_token
+    local easyexplorer_path
+    config_load easyexplorer
+    config_get easyexplorer_enabled main enabled
+    config_get easyexplorer_token main token
+    config_get easyexplorer_path main path
 
-    INPUT_JSON=`uci export_json easyexplorer`
-    json_load "$INPUT_JSON"
-    json_select "easyexplorer"
-    json_select "1"
+    status=`pidof easyexplorer`
+    router_id=`$APP_ROOT/easyexplorer -v`
 
-    if [ "$easyexplorer_status"x = "2"x ];then
-        json_add_string "status" "start"
-    else
-        json_add_string "status" "stop"
-    fi
-    json_dump
+    echo '{"status":"' ${status} '","router_id":"' $router_id '","token":"' $easyexplorer_token '","path":"' $path '","enabled":"' $easyexplorer_enabled '"}'
 }
 
 on_post() {
     local easyexplorer_enabled
     local easyexplorer_token
+    local easyexplorer_path
 
     json_load "$INPUT_JSON"
-    json_select "easyexplorer"
-    json_select "1"
     json_get_var easyexplorer_enabled "enabled"
     json_get_var easyexplorer_token "token"
+    json_get_var easyexplorer_path "path"
+    uci -q batch <<-EOT
+     set easyexplorer.enabled=$easyexplorer_enabled
+     set easyexplorer.token=$easyexplorer_token
+     set easyexporer.path=$easyexplorer_path
+    EOT
 
-    app_log $easyexplorer_enabled
-    app_log $easyexplorer_token
     if [ "$easyexplorer_enabled"x = "1"x ]; then
         killall easyexplorer > /dev/null 2>&1
-        $APP_ROOT/bin/easyexplorer -u $easyexplorer_token -d > /dev/null 2>&1
-        json_add_string "status" ""
-        json_dump|app_save_cfg
+        start-stop-daemon -S -b -q -x $APP_ROOT/bin/easyexplorer -u $easyexplorer_token -share $easyexplorer_path -c /tmp/ee >/dev/null
 
-        # mark as start
-        json_add_string "status" "start"
-        json_dump
+        uci commit
+        on_get
     elif [ "$easyexplorer_enabled"x = "0"x ]; then
         killall easyexplorer > /dev/null 2>&1
-        json_add_string "status" ""
-        json_dump|app_save_cfg
 
-        # mark as stop
-        json_add_string "status" "stop"
-        json_dump
+        uci commit
+        on_get
     else
         echo '{"status": "json_parse_failed"}'
     fi
@@ -57,12 +53,14 @@ on_post() {
 on_start() {
     local easyexplorer_enabled
     local easyexplorer_token
+    local easyexplorer_path
     config_load easyexplorer
     config_get easyexplorer_enabled main enabled
     config_get easyexplorer_token main token
+    config_get easyexplorer_path main path
     if [ "$easyexplorer_enabled"x = "1"x ]; then
         killall easyexplorer > /dev/null 2>&1
-        $APP_ROOT/bin/easyexplorer -u $easyexplorer_token -d > /dev/null 2>&1
+        start-stop-daemon -S -b -q -x $APP_ROOT/bin/easyexplorer -u $easyexplorer_token -share $easyexplorer_path -c /tmp/ee >/dev/null
     else
         killall easyexplorer > /dev/null 2>&1
     fi
@@ -70,18 +68,6 @@ on_start() {
 
 on_stop() {
     killall easyexplorer > /dev/null 2>&1
-}
-
-on_status() {
-    easyexplorer_status=`pidof easyexplorer|wc -w`
-    easyexplorer_pid=`pidof easyexplorer`
-    easyexplorer_version=`$APP_ROOT"/bin/easyexplorer" -v`
-    easyexplorer_route_id=`$APP_ROOT"/bin/easyexplorer" -w | awk '{print $2}'`
-    if [ "$easyexplorer_status"x = "2"x ];then
-        echo 进程运行正常！版本：${easyexplorer_version} 路由器ID：${easyexplorer_route_id} （PID：$easyexplorer_pid）
-    else
-        echo \<em\>【警告】：进程未运行！\<\/em\> 版本：${easyexplorer_version} 路由器ID：${easyexplorer_route_id}
-    fi
 }
 
 case $ACTION in
@@ -95,9 +81,10 @@ get)
     on_get
     ;;
 installed)
+    app_init_cfg '{"easyexplorer":[{"_id":"main","enabled":"0","token":"none","path":"/tmp/ee"}]}'
     ;;
 status)
-    on_status
+    on_get
     ;;
 stop)
     on_stop
